4f14700b2e885bc8de98c4bc7a0f3871f80f9376
ToxicSlash <49826496+ToxicSlash@users.noreply.github.com>
2026-09-01 12:47:44 +1000

    Expand building output sanity tests

--- src/test/java/com/example/mcatowns/town/BuildingPerformanceTest.java
+++ src/test/java/com/example/mcatowns/town/BuildingPerformanceTest.java
@@ -9,12 +9,14 @@ class BuildingPerformanceTest {
     void outputUsesTierWorkersFurnitureAndSynergies() {
         assertEquals(54, BuildingPerformance.outputFromFactors(1, 50, 2, 1));
         assertEquals(130, BuildingPerformance.outputFromFactors(3, 100, 20, 20));
+        assertEquals(80, BuildingPerformance.outputFromFactors(2, 75, 1, 0));
     }
 
     @Test
     void outputClampsUnsafeInputs() {
         assertEquals(0, BuildingPerformance.outputFromFactors(-5, -20, -2, -3));
         assertEquals(130, BuildingPerformance.outputFromFactors(50, 500, 500, 500));
+        assertEquals(0, BuildingPerformance.outputFromFactors(3, 0, 5, 2));
     }
 
     @Test
@@ -22,5 +24,7 @@ class BuildingPerformanceTest {
         assertEquals(3, BuildingPerformance.roundOutput(3, 110));
         assertEquals(4, BuildingPerformance.roundOutput(3, 117));
         assertEquals(11, BuildingPerformance.roundOutput(10, 105));
+        assertEquals(4, BuildingPerformance.roundOutput(7, 50));
+        assertEquals(0, BuildingPerformance.roundOutput(-10, 150));
     }
 }
